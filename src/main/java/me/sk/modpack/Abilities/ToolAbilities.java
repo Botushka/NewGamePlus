121b4b250d0cd87c6e24552ad53a3a52ac0835c2
BuildTools <91798274+Botushka@users.noreply.github.com>
2022-12-20 17:42:32 +0200

    4.0

--- src/main/java/me/sk/modpack/Abilities/ToolAbilities.java
+++ src/main/java/me/sk/modpack/Abilities/ToolAbilities.java
@@ -14,11 +14,9 @@ import java.util.Map;
 public class ToolAbilities implements Listener{
 
         Map<String, Long> cooldownStrenght = new HashMap<>();
-        Map<String, Long> cooldownMining = new HashMap<>();
-        Map<String, Long> cooldownAxe = new HashMap<>();
-        Map<String, Long> cooldownShovel = new HashMap<>();
+        Map<String, Long> cooldownSpeed = new HashMap<>();
         PotionEffect strenght = new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 100, 0);
-        PotionEffect Speed = new PotionEffect(PotionEffectType.FAST_DIGGING, 500, 2);
+        PotionEffect Speed = new PotionEffect(PotionEffectType.FAST_DIGGING, 500, 1);
 
     /**
      Strenght ability for diamond sword
@@ -29,119 +27,44 @@ public class ToolAbilities implements Listener{
             if (player.getInventory().getItemInMainHand().getType() == Material.DIAMOND_SWORD) {
                 if (e.getAction().equals(Action.LEFT_CLICK_AIR) == e.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
                     {
-                        if (player.hasPermission("invs")) {
-                            if (cooldownStrenght.containsKey(player.getName())) {
-
-                                if (cooldownStrenght.get(player.getName()) > System.currentTimeMillis()) {
-                                    long timeleft = (cooldownStrenght.get(player.getName()) - System.currentTimeMillis()) / 1000;
-                                    player.sendMessage(ChatColor.GOLD + "Ability will be ready in " + timeleft);
-                                    return;
-                                }
-                            }
-
-                            cooldownStrenght.put(player.getName(), System.currentTimeMillis() + (50 * 1000));
-                            player.addPotionEffect(strenght);
-                        }
-                    }
-
-                }
-
-
-            } else e.setCancelled(false);
-
-        }
-
-    /**
-     Faster mining ability for diamond pickaxe
-     */
-    @EventHandler
-    public void onRightPickaxe(PlayerInteractEvent e) {
-        Player player = e.getPlayer();
-        if (player.getInventory().getItemInMainHand().getType() == Material.DIAMOND_PICKAXE) {
-            if (e.getAction().equals(Action.LEFT_CLICK_AIR) == e.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
-                {
-                    if (player.hasPermission("invs")) {
-                        if (cooldownMining.containsKey(player.getName())) {
-
-                            if (cooldownMining.get(player.getName()) > System.currentTimeMillis()) {
-                                long timeleft = (cooldownMining.get(player.getName()) - System.currentTimeMillis()) / 1000;
+                        if (!(player.hasPermission("tools.abilities"))) {return;}
+                        if (cooldownStrenght.containsKey(player.getName())) {
+                            if (cooldownStrenght.get(player.getName()) > System.currentTimeMillis()) {
+                                long timeleft = (cooldownStrenght.get(player.getName()) - System.currentTimeMillis()) / 1000;
                                 player.sendMessage(ChatColor.GOLD + "Ability will be ready in " + timeleft);
                                 return;
+                                }
                             }
-                        }
-
-                        cooldownMining.put(player.getName(), System.currentTimeMillis() + (50 * 1000));
-                        player.addPotionEffect(Speed);
+                        cooldownStrenght.put(player.getName(), System.currentTimeMillis() + (50 * 1000));
+                        player.addPotionEffect(strenght);
                     }
                 }
-
-            }
-
-
-        } else e.setCancelled(false);
-
-    }
-
+            } else e.setCancelled(false);
+        }
     /**
      Faster cutting ability for diamond axe
      */
     @EventHandler
-    public void onRightAxe(PlayerInteractEvent e) {
+    public void onRightSpeed(PlayerInteractEvent e) {
         Player player = e.getPlayer();
-        if (player.getInventory().getItemInMainHand().getType() == Material.DIAMOND_AXE) {
+        if ((player.getInventory().getItemInMainHand().getType() == Material.DIAMOND_AXE) ||
+                (player.getInventory().getItemInMainHand().getType() == Material.DIAMOND_PICKAXE) ||
+                (player.getInventory().getItemInMainHand().getType() == Material.DIAMOND_SHOVEL)){
             if (e.getAction().equals(Action.LEFT_CLICK_AIR) == e.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
                 {
-                    if (player.hasPermission("invs")) {
-                        if (cooldownAxe.containsKey(player.getName())) {
-
-                            if (cooldownAxe.get(player.getName()) > System.currentTimeMillis()) {
-                                long timeleft = (cooldownAxe.get(player.getName()) - System.currentTimeMillis()) / 1000;
-                                player.sendMessage(ChatColor.GOLD + "Ability will be ready in " + timeleft);
+                    if (!(player.hasPermission("tools.abilities"))){return;}
+                    if (cooldownSpeed.containsKey(player.getName())) {
+                        if (cooldownSpeed.get(player.getName()) > System.currentTimeMillis()) {
+                            long timeleft = (cooldownSpeed.get(player.getName()) - System.currentTimeMillis()) / 1000;
+                            player.sendMessage(ChatColor.GOLD + "Ability will be ready in " + timeleft);
                                 return;
                             }
                         }
-
-                        cooldownAxe.put(player.getName(), System.currentTimeMillis() + (50 * 1000));
+                        cooldownSpeed.put(player.getName(), System.currentTimeMillis() + (50 * 1000));
                         player.addPotionEffect(Speed);
-                    }
                 }
-
             }
-
-
         } else e.setCancelled(false);
-
-    }
-
-    /**
-     Faster mining ability for diamond shovel
-     */
-    @EventHandler
-    public void onRightShovel(PlayerInteractEvent e) {
-        Player player = e.getPlayer();
-        if (player.getInventory().getItemInMainHand().getType() == Material.DIAMOND_SHOVEL) {
-            if (e.getAction().equals(Action.LEFT_CLICK_AIR) == e.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
-                {
-                    if (player.hasPermission("invs")) {
-                        if (cooldownShovel.containsKey(player.getName())) {
-
-                            if (cooldownShovel.get(player.getName()) > System.currentTimeMillis()) {
-                                long timeleft = (cooldownShovel.get(player.getName()) - System.currentTimeMillis()) / 1000;
-                                player.sendMessage(ChatColor.GOLD + "Ability will be ready in " + timeleft);
-                                return;
-                            }
-                        }
-
-                        cooldownShovel.put(player.getName(), System.currentTimeMillis() + (50 * 1000));
-                        player.addPotionEffect(Speed);
-                    }
-                }
-
-            }
-
-
-        } else e.setCancelled(false);
-
     }
 }
 
